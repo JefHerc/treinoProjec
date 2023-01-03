bab300ead4c3dd481790514d3b6704a0f3eb4158
JefHerc <jeferson.herculano@outlook.com>
2023-01-03 00:12:42 -0300

    implementacao h2

--- src/main/java/br/com/treion/dao/Dao.java
+++ src/main/java/br/com/treion/dao/Dao.java
@@ -6,38 +6,30 @@ import java.sql.SQLException;
 
 public class Dao {
 	private Connection conexao = null;
-	
+	private static boolean bancoNaoInicializado = true;
+
 	public Dao() {
 		conectar();
 	}
-	
+
 	public void conectar() {
+		StringBuilder url = new StringBuilder("jdbc:h2:mem:treino;");
+		
+		if(bancoNaoInicializado) {
+			url.append("INIT=runscript from 'classpath:Inicializar-H2.sql';");
+		}
 		try {
-			conexao =  DriverManager.getConnection("jdbc:mysql://localhost/avaliacao?useTimezone=true&serverTimezone=UTC", 
-					"root", "root");
+			conexao =  DriverManager.getConnection(url.toString());
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public Connection getConexao() throws SQLException {
 		if (conexao == null || conexao.isClosed()) {
 			conectar();
 		}
 		return conexao;
 	}
-	
-//	public static void main(String[] args) {
-//		try {
-//			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/avaliacao?useTimezone=true&serverTimezone=UTC", 
-//					"root", "root");
-//			System.out.println("funciona");
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
 }
-
-
